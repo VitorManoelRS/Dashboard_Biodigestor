27f3eef117a6e9aedecca4f5e1f3c9b43cf5b649
VitorManoelRS <vitor-mrs@hotmail.com>
2023-09-21 21:12:13 -0300

    implementação de uma nova página

--- script/script_geral.js
+++ script/script_geral.js
@@ -1,14 +1,11 @@
 //FUNÇÃO PARA PUXAR E TIRAR A SIDEBAR
 function openNav() {
     var bar = document.getElementById("mySidebar");
-    var main = document.getElementById("content");
     if (bar.style.left === "0px") {
         bar.style.left = "-250px";
-        main.style.marginLeft = "0";
     }
     else {
         bar.style.left = "0px";
-        main.style.marginLeft = "250px";
     }
 }
 
